c83b622631eff99b43443ea303128f9496827678
Anna Aurora <anna@annaaurora.eu>
2024-06-16 13:55:50 +0200

    Fixed illegal style element and redundant information in accessibility tree.

--- src/header.rs
+++ src/header.rs
@@ -4,10 +4,15 @@ use maud::{
     PreEscaped
 };
 
+struct Before {
+    content: char,
+    hidden: bool
+}
+
 struct MenuEntry {
     href: String,
     name: String,
-    before: Option<char>
+    before: Option<Before>
 }
 
 struct Menu {
@@ -21,7 +26,7 @@ impl Menu {
         }
     }
 
-    fn add_entry(&mut self, href: &str, name: &str, before: Option<char>) {
+    fn add_entry(&mut self, href: &str, name: &str, before: Option<Before>) {
         self.entries.push(
             MenuEntry { href: href.to_string(), name: name.to_string(), before }
         );
@@ -30,15 +35,15 @@ impl Menu {
 
 pub async fn header(nonce: &str) -> Markup {
     let mut menu = Menu::new();
-    menu.add_entry("/", "Home", Some('🏠'));
-    menu.add_entry("/blog/", "Blog", Some('📜'));
-    menu.add_entry("/art/", "Art portfolio", Some('🌠'));
+    menu.add_entry("/", "Home", Some(Before { content: '🏠', hidden: true }));
+    menu.add_entry("/blog/", "Blog", Some(Before { content: '📜', hidden: true }));
+    menu.add_entry("/art/", "Art portfolio", Some(Before { content: '🌠', hidden: true }));
     menu.add_entry("/services/", "Services", None);
-    menu.add_entry("/contact/", "Contact", Some('👋'));
-    menu.add_entry("/find-billy/", "Find Billy!", Some('🤖'));
-    menu.add_entry("/linux-journey/", "Linux journey", Some('🐧'));
-    menu.add_entry("/license/", "License", Some('©'));
-    menu.add_entry("https://codeberg.org/annaaurora/aaura-w3-strawb", "Source code", Some('📦'));
+    menu.add_entry("/contact/", "Contact", Some(Before { content: '👋', hidden: true }));
+    menu.add_entry("/find-billy/", "Find Billy!", Some(Before { content: '🤖', hidden: false }));
+    menu.add_entry("/linux-journey/", "Linux journey", Some(Before { content: '🐧', hidden: true }));
+    menu.add_entry("/license/", "License", Some(Before { content: '©', hidden: true }));
+    menu.add_entry("https://codeberg.org/annaaurora/aaura-w3-strawb", "Source code", Some(Before { content: '📦', hidden: true }));
     menu.add_entry("/atom.xml", "Atom feed", None);
 
     html! {
@@ -46,18 +51,16 @@ pub async fn header(nonce: &str) -> Markup {
             img #banner src="/static/banner-text-to-path.svg" alt="The name “Anna Aurora” in the font Comic Neue, a font looking similar to Comic Sans. “Anna” rotated 2° counter-clockwise and is placed over “Aurora” which is rotated by 2° clockwise. Both words are colored in the same gradient, starting with a light purple at the top left and ending in a light pink at the bottom right.";
             nav {
                 @for entry in &menu.entries {
-                    a href=(entry.href) { (entry.name) }
-                }
-                style nonce=(nonce) {
-                    @for (i, entry) in menu.entries.iter().enumerate() {
-                        @match entry.before {
-                            Some(content) => {
-                                (PreEscaped(
-                                    format!("header > nav a:nth-child({}) {{ --before: '{}'; }} ", i + 1, content)
-                                ))
-                            },
+                    a href=(entry.href) {
+                        @match &entry.before {
+                            Some(b) => {
+                                span aria-hidden=(b.hidden) {
+                                    (b.content)
+                                }
+                            }
                             None => {}
                         }
+                        (entry.name)
                     }
                 }
             }
