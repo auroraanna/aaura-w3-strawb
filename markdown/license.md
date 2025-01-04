a1615057bfef7a7a3b47f3424c5a80baf2f4d1ff
Anna Aurora <anna@annaaurora.eu>
2025-01-04 17:37:54 +0100

    Exchanged unicode emojis in nav bar for images.

--- markdown/license.md
+++ markdown/license.md
@@ -7,3 +7,8 @@ By default everything on this website, excluding websites hosted on subdomains, 
 
 - the Comic Neue Bold font served at `/fonts/ComicNeue-Bold` licensed under the [SIL Open Font License 1.1](https://openfontlicense.org/)
 - the stylesheet served at `/static/global.css` licensed under the [Unlicense](https://unlicense.org/)
+- `/static-src/xenia.png` was drawn by [RiniDisc](https://meow.social/@rinidisc) (modified works `/static-src/xenia_icon.xcf`, `/static/xenia_icon.avif`, `/static/xenia_icon.png`). The license is unclear.
+- `/static/mutant-standard_sparkles.svg` was made by [Caius Nocturne](https://nocturne.works/) and is licensed under [CC BY-NC-SA 4.0](https://creativecommons.org/licenses/by-nc-sa/4.0/).
+- `/static-src/neofox_home.png`, `/static/neofox_home.png`, `/static/neofox_home.webp` are licensed under [CC BY-NC-SA 4.0](https://creativecommons.org/licenses/by-nc-sa/4.0/). They were made with [Neomojimixer](https://neomojimixer.com) which sources from [various emoji sets](https://neomojimixer.com/imprint.html#sources).
+- `/static/neofox_wink.png` and `/static/neofox_wink.webp` are modified versions of the neofox\_wink emoji from the Neofox emoji set by [Volpeon](https://volpeon.ink), licensed under [CC BY-NC-SA 4.0](https://creativecommons.org/licenses/by-nc-sa/4.0/).
+- `/static/neobot.png` and `/static/neobot.webp` are modified versions of the neobot emoji from [the Neobot emoji set](https://github.com/SymTrkl/emoji/tree/neobot). The neobot emoji is licensed under [CC BY-NC-SA 4.0](https://creativecommons.org/licenses/by-nc-sa/4.0/). Attribution unclear.
--- src/header.rs
+++ src/header.rs
@@ -1,69 +1,70 @@
 use maud::{
     html,
     Markup,
-    PreEscaped
 };
 
-struct Before {
-    content: char,
-    hidden: bool
-}
-
-struct MenuEntry {
-    href: String,
-    name: String,
-    before: Option<Before>
-}
-
-struct Menu {
-    entries: Vec<MenuEntry>
-}
-
-impl Menu {
-    fn new() -> Self {
-        Menu {
-            entries: Vec::new(),
-        }
-    }
-
-    fn add_entry(&mut self, href: &str, name: &str, before: Option<Before>) {
-        self.entries.push(
-            MenuEntry { href: href.to_string(), name: name.to_string(), before }
-        );
-    }
-}
-
 pub async fn header(nonce: &str) -> Markup {
-    let mut menu = Menu::new();
-    menu.add_entry("/", "Home", Some(Before { content: '🏠', hidden: true }));
-    menu.add_entry("/blog/", "Blog", Some(Before { content: '📜', hidden: true }));
-    menu.add_entry("/art/", "Art portfolio", Some(Before { content: '🌠', hidden: true }));
-    menu.add_entry("/services/", "Services", None);
-    menu.add_entry("/contact/", "Contact", Some(Before { content: '👋', hidden: true }));
-    menu.add_entry("/find-billy/", "Find Billy!", Some(Before { content: '🤖', hidden: false }));
-    menu.add_entry("/linux-journey/", "Linux journey", Some(Before { content: '🐧', hidden: true }));
-    menu.add_entry("https://ring.annaaurora.eu/", "ring", Some(Before { content: '🪐', hidden: false }));
-    menu.add_entry("/license/", "License", Some(Before { content: '©', hidden: true }));
-    menu.add_entry("https://codeberg.org/annaaurora/aaura-w3-strawb", "Source code", Some(Before { content: '📦', hidden: true }));
-    menu.add_entry("/atom.xml", "Atom feed", None);
-    menu.add_entry("/feed.json", "JSON Feed", None);
-
     html! {
         header {
             img #banner src="/static/banner-text-to-path.svg" alt="The name “Anna Aurora” in the font Comic Neue, a font looking similar to Comic Sans. “Anna” rotated 2° counter-clockwise and is placed over “Aurora” which is rotated by 2° clockwise. Both words are colored in the same gradient, starting with a light purple at the top left and ending in a light pink at the bottom right.";
             nav {
-                @for entry in &menu.entries {
-                    a href=(entry.href) {
-                        @match &entry.before {
-                            Some(b) => {
-                                span aria-hidden=(b.hidden) {
-                                    (b.content)
-                                }
-                            }
-                            None => {}
-                        }
-                        (entry.name)
+                a href="/" {
+                    picture {
+                        source srcset="/static/neofox_home.webp";
+                        img src="/static/neofox_home.png" alt="";
                     }
+                    "Home"
+                }
+                a href="/blog/" {
+                    "Blog"
+                }
+                a href="/art/" { 
+                    img src="/static/mutant-standard_sparkles.svg" alt="";
+                    "Art portfolio"
+                }
+                a href="/services/" {
+                    "Services"
+                }
+                a href="/contact/" {
+                    picture {
+                        source srcset="/static/neofox_wink.webp";
+                        img src="/static/neofox_wink.png" alt="";
+                    }
+                    "Contact"
+                }
+                a href="/find-billy/" {
+                    picture {
+                        source srcset="/static/neobot.webp";
+                        img src="/static/neobot.png" alt="a neobot";
+                    }
+                    "Find Billy!"
+                }
+                a href="/linux-journey/" {
+                    picture {
+                        source srcset="/static/xenia_icon.avif";
+                        img src="/static/xenia_icon.png" alt="";
+                    }
+                    "Linux journey"
+                }
+                a href="https://ring.annaaurora.eu/" {
+                    picture {
+                        source srcset="/static/ring_0005.webp";
+                        img src="/static/ring_0005.png" alt="a smooth, golden, thin toroid";
+                    }
+                    "ring"
+                }
+                a href="/license/" {
+                    span aria-hidden="true" { "©" }
+                    "License"
+                }
+                a href="https://codeberg.org/annaaurora/aaura-w3-strawb" {
+                    "Source code"
+                }
+                a href="/atom.xml" {
+                    "Atom feed"
+                }
+                a href="/feed.json" {
+                    "JSON Feed"
                 }
             }
         }
